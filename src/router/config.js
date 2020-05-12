6e593dcc386ef2abfcf3bf7bbcc331a08d86a8de
zhouwentai <moon@B-J5B9G8WN-1714.local>
2020-05-12 17:22:32 +0800

    fix

--- src/router/config.js
+++ src/router/config.js
@@ -2,40 +2,40 @@ import { Index } from '../page/reactHooks-learn'
 import { Children } from '../page/reactHooks-learn/children'
 import TestReudex from '../page/test-redux'
 import { Project_time_line } from "../page/project-time-line";
-import TesthooksRudex  from '../page/react-hooks-redux'
+import TesthooksRudex from '../page/react-hooks-redux'
 export const config = [
-    {
-        path: '/react_hook',
-        exact: true,
-        component: Index,
-        routes: [
-            // {
-            //     path: '/parent',
-            //     redirect: '/home/parent',
-            // },
-            // {
-            //     name: 'parent',
-            //     path: '/react_hook/parent',
-            //     exact: true,
-            //     component: Parent,
-            // },
-        ]
-
-    },
-    {
-        path: '/children',
-        component: Children,
-    },
-    {
+  // {
+  //   path: '/',
+  //   exact: true,
+  //   component: Index,
+  // },
+  {
+    path: '/react_hook',
+    exact: true,
+    component: Index,
+    routes: [
+      {
         path: '/project_time_line',
         component: Project_time_line,
-    },
-    {
-        path: '/testRudex',
-        component: TestReudex,
-    },
-    {
-      path: '/testhooksRudex',
-      component: TesthooksRudex,
+      },
+    ]
+
+  },
+  {
+    path: '/project_time_line',
+    component: Project_time_line,
+  },
+  {
+    path: '/children',
+    component: Children,
+  },
+
+  {
+    path: '/testRudex',
+    component: TestReudex,
+  },
+  {
+    path: '/testhooksRudex',
+    component: TesthooksRudex,
   }
 ]
