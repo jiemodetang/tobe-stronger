5efcbc9bb25e0258d1ca861ab496352d8c1f623b
zhouwentai <moon@B-J5B9G8WN-1714.local>
2020-05-19 16:29:32 +0800

    fix

--- src/page/markdown-editing/index.js
+++ src/page/markdown-editing/index.js
@@ -1,34 +1,137 @@
 
 import React, { useEffect, useState } from 'react';
-import ReactMarkdown from 'react-markdown'
 import axiosApi from '../../utils/axios'
+import BraftEditor from 'braft-editor'
+import 'braft-editor/dist/index.css'
+// https://braft.margox.cn/demos/basic
+export default class Markdown_editing extends React.Component {
 
+    state = {
+        editorState: BraftEditor.createEditorState()
+    }
 
+    handleChange = (editorState) => {
+        this.setState({ editorState })
+    }
+    componentDidMount() {
+        // axiosApi.get('../mock/markdownTest.md', {})
+        //     .then(res => {
+        //         if (res.status === 200) {
+        //             console.log(res.data);
 
-const Markdown_editing = () => {
-    const [childCount, setChildCount] = useState('')
+        //             this.setState({
+        //                 editorState:BraftEditor.createEditorState( res.data) 
+        //             })
+        //         }
+        //     })
+    }
+    preview = () => {
 
-    useEffect(() => {
-        // axios 接口返回值给子组件state赋值，并渲染
-        axiosApi.get('../mock/markdownTest.md', {})
-            .then(res => {
-                if (res.status === 200) {
-                    console.log(res.data)
-                    setChildCount(res.data)
-                }
-            })
-    }, [])
-    return (
-        <div>
-            11
-            <ReactMarkdown
-                source={"## Tables?"}
-                escapeHtml={true}
-               
-            />
+        if (window.previewWindow) {
+            window.previewWindow.close()
+        }
 
-        </div>
-    )
+        window.previewWindow = window.open()
+        window.previewWindow.document.write(this.buildPreviewHtml())
+        window.previewWindow.document.close()
+
+    }
+
+    buildPreviewHtml() {
+
+        return `
+            <!Doctype html>
+            <html>
+              <head>
+                <title>Preview Content</title>
+                <style>
+                  html,body{
+                    height: 100%;
+                    margin: 0;
+                    padding: 0;
+                    overflow: auto;
+                    background-color: #f1f2f3;
+                  }
+                  .container{
+                    box-sizing: border-box;
+                    width: 1000px;
+                    max-width: 100%;
+                    min-height: 100%;
+                    margin: 0 auto;
+                    padding: 30px 20px;
+                    overflow: hidden;
+                    background-color: #fff;
+                    border-right: solid 1px #eee;
+                    border-left: solid 1px #eee;
+                  }
+                  .container img,
+                  .container audio,
+                  .container video{
+                    max-width: 100%;
+                    height: auto;
+                  }
+                  .container p{
+                    white-space: pre-wrap;
+                    min-height: 1em;
+                  }
+                  .container pre{
+                    padding: 15px;
+                    background-color: #f1f1f1;
+                    border-radius: 5px;
+                  }
+                  .container blockquote{
+                    margin: 0;
+                    padding: 15px;
+                    background-color: #f1f1f1;
+                    border-left: 3px solid #d1d1d1;
+                  }
+                </style>
+              </head>
+              <body>
+                <div class="container">${this.state.editorState.toHTML()}</div>
+              </body>
+            </html>
+          `
+
+    }
+
+    render() {
+
+        const excludeControls = [
+            'letter-spacing',
+            'line-height',
+            'clear',
+            'headings',
+            'list-ol',
+            'list-ul',
+            'remove-styles',
+            'superscript',
+            'subscript',
+            'hr',
+            'text-align'
+        ]
+
+        const extendControls = [
+            {
+                key: 'custom-button',
+                type: 'button',
+                text: '预览',
+                onClick: this.preview
+            }
+        ]
+
+        return (
+            <div className="editor-wrapper">
+                <BraftEditor
+                    value={this.state.editorState}
+                    onChange={this.handleChange}
+                    excludeControls={excludeControls}
+                    extendControls={extendControls}
+                    contentStyle={{ height: 400 }}
+                />
+            </div>
+        )
+
+    }
 
 }
-export default Markdown_editing 
